f9729fa5f475102f01bbdc5b1f68a3b8a8e2e63b
ilfey <ilfey.global@gmail.com>
2022-10-18 03:25:28 +0700

    add `handleJPEG` and `handleGIF`

--- internal/app/img/img.go
+++ internal/app/img/img.go
@@ -1,6 +1,8 @@
 package img
 
 import (
+	"image/gif"
+	"image/jpeg"
 	"image/png"
 
 	"net/http"
@@ -17,6 +19,9 @@ func New() handlers.Handler {
 
 func (h *handler) Register(router *mux.Router) {
 	router.HandleFunc("/img/{x:[0-9]+}x{y:[0-9]+}.png", h.handlePNG())
+	router.HandleFunc("/img/{x:[0-9]+}x{y:[0-9]+}.jpeg", h.handleJPEG())
+	router.HandleFunc("/img/{x:[0-9]+}x{y:[0-9]+}.jpg", h.handleJPEG())
+	router.HandleFunc("/img/{x:[0-9]+}x{y:[0-9]+}.gif", h.handleGIF())
 }
 
 func (h *handler) handlePNG() http.HandlerFunc {
@@ -38,5 +43,53 @@ func (h *handler) handlePNG() http.HandlerFunc {
 		// send image
 		w.WriteHeader(200)
 		png.Encode(w, img)
+	}
+}
+
+func (h *handler) handleJPEG() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// parse imageParams
+		params, code, err := parseImageParams(r)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		// create image
+		img, err := createImage(params)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		// send image
+		w.WriteHeader(200)
+		jpeg.Encode(w, img, &jpeg.Options{
+			Quality: 30,
+		})
+	}
+}
+
+func (h *handler) handleGIF() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// parse imageParams
+		params, code, err := parseImageParams(r)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		// create image
+		img, err := createImage(params)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		// send image
+		w.WriteHeader(200)
+		gif.Encode(w, img, &gif.Options{
+			NumColors: 256,
+		})
 	}
 }
