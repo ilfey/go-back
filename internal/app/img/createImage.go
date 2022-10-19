415ff50fcb06661b62a5ac990fc9426e1b9cbfa5
ilfey <ilfey.global@gmail.com>
2022-10-20 03:13:22 +0700

    add img generation

--- internal/app/img/createImage.go
+++ internal/app/img/createImage.go
@@ -1,6 +1,8 @@
 package img
 
 import (
+	"fmt"
+
 	"github.com/fogleman/gg"
 )
 
@@ -14,13 +16,36 @@ type imageParams struct {
 }
 
 func createImage(p *imageParams) (*gg.Context, error) {
+	W := float64(p.x)
+	H := float64(p.y)
 
 	ctx := gg.NewContext(p.x, p.y)
 
 	// set background
+	ctx.SetHexColor(p.bg)
+	ctx.Clear()
+
+	// set color and line width
+	ctx.SetHexColor(p.fg)
+	ctx.SetLineWidth(p.border)
+
 	// create border
+	ctx.DrawLine(0, 0, W, 0) // top
+	ctx.DrawLine(W, 0, W, H) // right
+	ctx.DrawLine(W, H, 0, H) // bottom
+	ctx.DrawLine(0, H, 0, 0) // left
+
 	// create x
+	ctx.DrawLine(0, 0, W/4, H/4)     // top left
+	ctx.DrawLine(W, 0, W-W/4, H/4)   // top right
+	ctx.DrawLine(W, H, W-W/4, H-H/4) // bottom right
+	ctx.DrawLine(0, H, W/4, H-H/4)   // bottom left
+
+	// draw lines
+	ctx.Stroke()
+
 	// set text
+	ctx.DrawStringAnchored(fmt.Sprintf("%dx%d", p.x, p.y), W/2, H/2, 0.5, 0.5)
 
 	return ctx, nil
 }
