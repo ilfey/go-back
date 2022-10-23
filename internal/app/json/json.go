808da683c75a897a2e9fffb4c9e3cd0b597ccd8f
ilfey <ilfey.global@gmail.com>
2022-10-24 00:48:58 +0700

    add `handleArrayOfString`

--- internal/app/json/json.go
+++ internal/app/json/json.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/XANi/loremipsum"
 	"github.com/gorilla/mux"
 	"github.com/ilfey/go-back/internal/app/handlers"
 )
@@ -14,6 +15,7 @@ type handler struct{}
 
 func New() handlers.Handler {
 	rand.Seed(time.Now().UnixNano())
+
 	return &handler{}
 }
 
@@ -21,15 +23,44 @@ func (h *handler) Register(router *mux.Router) {
 	// TODO implement routes
 	// router.Handle("/json/object", h.handleObject())
 	// router.Handle("/json/array/object", h.handleArrayOfObject())
-	// router.Handle("/json/array/string", h.handleArrayOfString())
+	router.Handle("/json/array/string", h.handleArrayOfString()) // queries: len=[1-100]
 	// router.Handle("/json/array/number", h.handleArrayOfNumber())
 	router.Handle("/json/array/boolean", h.handleArrayOfBoolean()) // queries: len=[1-100]
+}
+
+func (h *handler) handleArrayOfString() http.HandlerFunc {
+	li := loremipsum.New()
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		queries := r.URL.Query()
+
+		length, code, err := parseLegth(queries)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		arr := li.WordList(length)
+
+		// swap arr
+		rand.Shuffle(length, func(i, j int) {
+			arr[i], arr[j] = arr[j], arr[i]
+		})
+
+		// serialize array
+		j, _ := json.Marshal(arr)
+
+		w.WriteHeader(200)
+		w.Write(j)
+	}
 }
 
 func (h *handler) handleArrayOfBoolean() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		queries := r.URL.Query()
 
+		// parse length
 		length, code, err := parseLegth(queries)
 		if err != nil {
 			w.WriteHeader(code)
