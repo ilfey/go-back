21ddbe4cbbbb29fb7542a26463f068419b6d4ee8
ilfey <ilfey.global@gmail.com>
2022-11-02 20:59:36 +0700

    add `handleArrayOfNumber`

--- internal/app/json/json.go
+++ internal/app/json/json.go
@@ -23,8 +23,8 @@ func (h *handler) Register(router *mux.Router) {
 	// TODO implement routes
 	// router.Handle("/json/object", h.handleObject())
 	// router.Handle("/json/array/object", h.handleArrayOfObject())
-	router.Handle("/json/array/string", h.handleArrayOfString()) // queries: len=[1-100]
-	// router.Handle("/json/array/number", h.handleArrayOfNumber())
+	router.Handle("/json/array/string", h.handleArrayOfString())   // queries: len=[1-100]
+	router.Handle("/json/array/number", h.handleArrayOfNumber())   // queries: len=[1-100], min=[math.MinInt32-math.MaxInt32]
 	router.Handle("/json/array/boolean", h.handleArrayOfBoolean()) // queries: len=[1-100]
 }
 
@@ -34,6 +34,7 @@ func (h *handler) handleArrayOfString() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		queries := r.URL.Query()
 
+		// parse length
 		length, code, err := parseLegth(queries)
 		if err != nil {
 			w.WriteHeader(code)
@@ -50,6 +51,57 @@ func (h *handler) handleArrayOfString() http.HandlerFunc {
 
 		// serialize array
 		j, _ := json.Marshal(arr)
+
+		w.WriteHeader(200)
+		w.Write(j)
+	}
+}
+
+func (h *handler) handleArrayOfNumber() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		queries := r.URL.Query()
+
+		// parse length
+		length, code, err := parseLegth(queries)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		var j []byte
+
+		// parse min
+		min, code, err := parseIntMin(queries)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// parse max
+		max, code, err := parseIntMax(queries)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// check min and max values
+		code, err = checkIntMinMax(min, max)
+		if err != nil {
+			w.WriteHeader(code)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		arr := make([]int, length)
+
+		for index := 0; index < length; index++ {
+			arr[index] = rand.Intn(max-min+1) + min
+		}
+
+		j, _ = json.Marshal(arr)
 
 		w.WriteHeader(200)
 		w.Write(j)
--- internal/app/json/parsers.go
+++ internal/app/json/parsers.go
@@ -2,7 +2,13 @@ package json
 
 import (
 	"fmt"
+	"math"
 	"strconv"
+)
+
+const (
+	INT   = "int"
+	FLOAT = "float"
 )
 
 func parseQuery(q map[string][]string, p string) (val string, err error) {
@@ -25,6 +31,43 @@ func parseLegth(q map[string][]string) (length, code int, err error) {
 			err = fmt.Errorf("error: len value must be a number in the range [1-100]")
 			return
 		}
+	}
+	return
+}
+
+func parseIntMin(q map[string][]string) (max, code int, err error) {
+	if maxStr, _err := parseQuery(q, "min"); _err != nil {
+		max = 0
+	} else {
+		max, _err = strconv.Atoi(maxStr)
+		if _err != nil || max < math.MinInt32 || max > math.MaxInt32 {
+			code = 400
+			err = fmt.Errorf("error: min value must be a number in the range [%d-%d]", math.MinInt32, math.MaxInt32)
+			return
+		}
+	}
+	return
+}
+
+func parseIntMax(q map[string][]string) (max, code int, err error) {
+	if maxStr, _err := parseQuery(q, "max"); _err != nil {
+		max = 100
+	} else {
+		max, _err = strconv.Atoi(maxStr)
+		if _err != nil || max < math.MinInt32 || max > math.MaxInt32 {
+			code = 400
+			err = fmt.Errorf("error: max value must be a number in the range [%d-%d]", math.MinInt32, math.MaxInt32)
+			return
+		}
+	}
+	return
+}
+
+func checkIntMinMax(min, max int) (code int, err error) {
+	if min > max {
+		code = 412
+		err = fmt.Errorf("the min value cannot be greater than the max")
+		return
 	}
 	return
 }
