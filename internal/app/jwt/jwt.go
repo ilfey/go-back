6d190d53a566fc09b784b7e60b9f1794ca2b9e17
ilfey <ilfey.global@gmail.com>
2022-11-18 04:26:51 +0700

    add simple `handleLogin`

--- internal/app/jwt/jwt.go
+++ internal/app/jwt/jwt.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/ilfey/go-back/internal/app/handlers"
-	"github.com/ilfey/go-back/internal/app/store/models"
 	"github.com/ilfey/go-back/internal/app/store/sqlstore"
 )
 
@@ -23,23 +22,55 @@ func New(store *sqlstore.Store) handlers.Handler {
 
 func (h *handler) Register(router *mux.Router) {
 	router.HandleFunc("/jwt/register", h.handleRegister()).Methods(http.MethodPost)
+	router.HandleFunc("/jwt/login", h.handleLogin()).Methods(http.MethodPost)
 }
 
 func (h *handler) handleRegister() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var u models.User
-
-		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-			http.Error(w, "bad request", 400)
+		// parse user
+		u, err := parseUserFromBody(r)
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+		// create user
+		if err := h.store.User().Create(context.TODO(), u); err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
 			return
 		}
 
-		if err := h.store.User().Create(context.TODO(), &u); err != nil {
-			http.Error(w, "Bad Request", 400)
-			return
-		}
-
-		w.WriteHeader(200)
+		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte("success"))
+	}
+}
+
+func (h *handler) handleLogin() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// parse user
+		u, err := parseUserFromBody(r)
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+		// get user
+		userExists, err := h.store.User().FindByUsername(context.Background(), u.Username)
+		if err != nil {
+			http.Error(w, "user is not exists", http.StatusUnauthorized)
+			return
+		}
+		// compare passwords
+		if !userExists.ComparePassword(u.Password) {
+			// convert to json
+			j, err := json.Marshal(userExists)
+			if err != nil {
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+			}
+
+			w.WriteHeader(200)
+			w.Write(j)
+			return
+		}
+
+		http.Error(w, "password is not valid", http.StatusUnauthorized)
 	}
 }
