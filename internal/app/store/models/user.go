698c072fcc76ff8cf86bc5f6034850a5b7dc8220
ilfey <ilfey.global@gmail.com>
2022-11-22 22:34:09 +0700

    remove IsDeleted from json field

--- internal/app/store/models/user.go
+++ internal/app/store/models/user.go
@@ -11,7 +11,7 @@ type User struct {
 	Username  string `json:"username"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
-	IsDeleted bool
+	IsDeleted bool   `json:"-"`
 }
 
 func (u *User) BeforeCreate() error {
