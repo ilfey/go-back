cbaef5524cc6bcabe944bdf449d445c7fb83d8c5
ilfey <ilfey.global@gmail.com>
2022-12-03 10:23:44 +0000

    refactor `img` endpoints

--- internal/app/endpoints/img/parsers.go
+++ internal/app/endpoints/img/parsers.go
@@ -25,7 +25,7 @@ func parseImageParams(r *http.Request) (params *imageParams, code int, err error
 	// parse x
 	x, _err := strconv.Atoi(vars["x"])
 	if _err != nil || x == 0 || x > 2000 {
-		code = 412
+		code = http.StatusBadRequest
 		err = fmt.Errorf("error: width value not parsed. you can specify a value in the range [1-2000]")
 		return
 	}
@@ -33,7 +33,7 @@ func parseImageParams(r *http.Request) (params *imageParams, code int, err error
 	// parse y
 	y, _err := strconv.Atoi(vars["y"])
 	if _err != nil || y == 0 || y > 2000 {
-		code = 412
+		code = http.StatusBadRequest
 		err = fmt.Errorf("error: width value not parsed. you can specify a value in the range [1-2000]")
 		return
 	}
@@ -46,31 +46,32 @@ func parseImageParams(r *http.Request) (params *imageParams, code int, err error
 	} else {
 		border, err = strconv.Atoi(borderStr)
 		if err != nil || border <= 0 || border > 50 {
-			code = 400
+			code = http.StatusBadRequest
 			err = fmt.Errorf("error: border value must be a number in the range [1-50]")
 			return
 		}
 	}
 
 	//parse background
-	bg, _err := parseQuery(queries, "bg")
+	background, _err := parseQuery(queries, "bg")
 	if _err != nil {
-		bg = "fff"
+		background = "fff"
 	}
 
 	// parse foreground
-	fg, _err := parseQuery(queries, "fg")
+	foreground, _err := parseQuery(queries, "fg")
 	if _err != nil {
-		fg = "000"
+		foreground = "000"
 	}
 
 	params = &imageParams{
-		x:      x,
-		y:      y,
-		bg:     bg,
-		fg:     fg,
-		tan:    float64(y) / float64(x),
-		border: float64(border),
+		x:          x,
+		y:          y,
+		background: background,
+		foreground: foreground,
+		tan:        float64(y) / float64(x),
+		border:     float64(border),
+		imageType:  vars["type"],
 	}
 
 	return
