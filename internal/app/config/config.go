942f35f9d188c7a4fd9cc2294e14dd404dde8c8a
ilfey <ilfey.global@gmail.com>
2022-12-17 19:00:17 +0000

    add sqlite database

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,9 +1,10 @@
 package config
 
 type Config struct {
-	Address     string
-	LogLevel    string
-	DatabaseUrl string
-	Key         []byte
-	LifeSpan    int
+	Address      string
+	LogLevel     string
+	DatabaseUrl  string
+	DatabaseFile string
+	Key          []byte
+	LifeSpan     int
 }
